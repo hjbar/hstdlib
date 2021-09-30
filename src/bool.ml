5991d7cfc22a3780b6b56372b1aca3ef4d5d489a
hjbar <hjbar@protonmail.com>
2021-09-30 17:36:26 +0200

    Ajout de nouvelles fonctions

--- src/bool.ml
+++ src/bool.ml
@@ -3,13 +3,8 @@ type t = bool = | false | true
 let not b = if b then false else true
 
 let (&&) b1 b2 =
-  if b1 then
-    if b2 then
-      true
-    else
-      false
-  else
-    false
+  if b1 then b2
+  else false
 
 let (||) b1 b2 =
   if b1 then
@@ -27,3 +22,15 @@ let compare b1 b2 =
   if equal b1 b2 then 0
   else if b1 then 1
   else -1
+
+let to_int b =
+  if b then 1
+  else 0
+
+let to_float b =
+  if b then 1.
+  else 0.
+
+let to_string b =
+  if b then "true"
+  else "false"
